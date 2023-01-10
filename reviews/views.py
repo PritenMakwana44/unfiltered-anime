5df016ce0d37e688dddbc28eb20ecd52455244ea
PritenMakwana44 <priten.makwana7@gmail.com>
2023-01-10 21:49:15 +0000

    issues with links soemthings in both urls.py and views.py changed

--- reviews/views.py
+++ reviews/views.py
@@ -2,8 +2,16 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Review
 
-class ReviewList(generic.ListView):
-    model = Review
-    queryset = Review.objects.filter(status=1).order_by('-publish_date')
+
+class HomeView(requests):
+    model = Home
     template_name = 'index.html'
+
+
+
+class ReviewList(generic.ListView):
+    model = Reviews
+    queryset = Review.objects.filter(status=1).order_by('-publish_date')
+    template_name = 'review.html'
     paginate_by = 10
+    
