e28b1b8375f8570c130c74f870f10e5d946f3dd7
PritenMakwana44 <priten.makwana7@gmail.com>
2023-01-11 20:28:29 +0000

    index working and partial working review_detail.html

--- reviews/views.py
+++ reviews/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.views import generic
+from django.views import generic, View
 from .models import Review
 
 class ReviewList(generic.ListView):
@@ -8,3 +8,29 @@ class ReviewList(generic.ListView):
     template_name = 'index.html'
     paginate_by = 10
     
+class ReviewDetail(View):
+
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Review.objects.filter(status=1)
+        review = get_object_or_404(queryset, slug=slug)
+        comments = review.comments.filter(approved=True).order_by('comment_date')
+        upvotes = False
+        #if review.upvotes.filter(id=self.request.user.id).exists():
+            #upvotes = True
+        downvotes = False
+        #if review.upvote.filter(id=self.request.user.id).exists():
+            #downvotes = True
+        
+        return render(
+            request,
+            "review_detail.html",
+            {
+                "review": review,
+                "comments": comments,
+                "upvotes": upvotes,
+                "downvotes": downvotes
+            },
+
+
+        )
