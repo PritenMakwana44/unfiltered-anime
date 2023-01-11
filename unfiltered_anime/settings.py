f7be9b93ec45ef29192b1b8ec1edb3410fb3411f
PritenMakwana44 <priten.makwana7@gmail.com>
2023-01-11 21:17:34 +0000

    installed allauth edited urls and settings

--- unfiltered_anime/settings.py
+++ unfiltered_anime/settings.py
@@ -41,12 +41,21 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.sites'
+    'allauth.account',
+    'allauth',
+    'allauth.socialaccount',
     'cloudinary_storage',
     'django.contrib.staticfiles',
     'cloudinary',
     'django_summernote',
     'reviews',
 ]
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
