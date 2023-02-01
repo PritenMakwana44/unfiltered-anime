aa2b91f702ca4faf88ed5a693d73e24a99b44c6f
PritenMakwana44 <priten.makwana7@gmail.com>
2023-02-01 22:05:55 +0000

    Some responsiveness fixed for menu button  and js moved to js file

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,8 @@
-
+window.addEventListener('load', function () {
+    setTimeout(function () {
+      let messages = document.getElementById('msg');
+      let alert = new bootstrap.Alert(messages);
+      alert.close();
+    }, 2500);
+  });
 
