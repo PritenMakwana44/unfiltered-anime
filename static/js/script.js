3faffa0dccf529ed77415bcec8381a8cf5b31a1e
PritenMakwana44 <priten.makwana7@gmail.com>
2023-02-13 23:14:38 +0000

    final commit

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,7 @@
 // Get the button:
 let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 1x from the top of the document, show the button
+// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -14,5 +14,5 @@ function scrollFunction() {
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+  document.documentElement.scrollTop = 0; // 
 }
